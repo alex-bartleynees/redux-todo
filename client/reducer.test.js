ba7d60dcddba6ba717b140002b3d969761f7524d
Alex Bartley Nees <alexbartleynees@gmail.com>
2021-04-19 19:32:34 +1200

    Add toggle todo and test

--- client/reducer.test.js
+++ client/reducer.test.js
@@ -21,4 +21,44 @@ describe('reducer tests', () => {
 
     expect(todos(stateBefore, action)).toEqual(stateAfter)
 })
+
+    test('test toggle todo', () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go shopping',
+            completed: false
+        }
+    ]
+
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    }
+
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go shopping',
+            completed: true
+        }
+    ]
+
+    Object.freeze(stateBefore)
+    Object.freeze(action)
+
+    expect(todos(stateBefore, action)).toEqual(stateAfter)
+
+
+    })
 })
